1cd031019833a9f2f7d39e650558e8faf628f4b5
Milos Dakic <milos.dakic@dt.com.au>
2017-08-17 21:26:19 +1000

    Implement interface

--- src/Eloquent/Option.php
+++ src/Eloquent/Option.php
@@ -2,15 +2,39 @@
 
 namespace StallNinja\Product\Eloquent;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
 use StallNinja\Product\Contracts\Option as OptionInterface;
-use Illuminate\Database\Eloquent\Model as Entity;
 
-class Option extends Entity implements OptionInterface
+class Option extends Model implements OptionInterface
 {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
     protected $table = "product_options";
 
-    public function values()
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function values() : Collection
     {
         $this->hasMany(app('StallNinja\Product\Eloquent\OptionValue'));
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasValues() : bool
+    {
+        return $this->values()->isEmpty();
     }
 }
