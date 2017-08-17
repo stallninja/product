aee38eb4460c1ce8ad30f68500d4f285254ab957
Milos Dakic <milos.dakic@dt.com.au>
2017-08-17 21:12:58 +1000

    Return types & docs

--- src/Eloquent/Variant.php
+++ src/Eloquent/Variant.php
@@ -2,19 +2,38 @@
 
 namespace StallNinja\Product\Eloquent;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
 use StallNinja\Product\Contracts\Variant as VariantInterface;
-use Illuminate\Database\Eloquent\Model as Entity;
 
-class Variant extends Entity implements VariantInterface
+class Variant extends Model implements VariantInterface
 {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
     protected $table = "product_variants";
 
-    public function product()
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function product() : Model
     {
         return $this->belongsTo(app('StallNinja\Product\Eloquent\Product'));
     }
 
-    public function optionValues()
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function optionValues() : Collection
     {
         return $this->hasMany(app('StallNinja\Product\Eloquent\OptionValue'));
     }
