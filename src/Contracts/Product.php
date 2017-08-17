4c69d6e1d36dc1e8f0c7c7b3b6363a3e998b84da
Milos Dakic <milos.dakic@dt.com.au>
2017-08-17 21:13:15 +1000

    Contract implementation, first pass

--- src/Contracts/Product.php
+++ src/Contracts/Product.php
@@ -2,6 +2,37 @@
 
 namespace StallNinja\Product\Contracts;
 
+use Illuminate\Database\Eloquent\Collection;
+
 interface Product
 {
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function variants() : Collection;
+
+    /**
+     * @return bool
+     */
+    public function hasVariants() : bool;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function attributes() : Collection;
+
+    /**
+     * @return bool
+     */
+    public function hasAttributes() : bool;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function options() : Collection;
+
+    /**
+     * @return bool
+     */
+    public function hasOptions() : bool;
 }
