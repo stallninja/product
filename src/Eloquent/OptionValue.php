31caffbcd62e63ce967d7ef2200844f62ef6378d
Milos Dakic <milos.dakic@dt.com.au>
2017-08-17 21:25:51 +1000

    Docs & remove alias to model

--- src/Eloquent/OptionValue.php
+++ src/Eloquent/OptionValue.php
@@ -2,10 +2,15 @@
 
 namespace StallNinja\Product\Eloquent;
 
+use Illuminate\Database\Eloquent\Model;
 use StallNinja\Product\Contacts\OptionValue as OptionValueInterface;
-use Illuminate\Database\Eloquent\Model as Entity;
 
-class OptionValue extends Entity implements OptionValueInterface
+class OptionValue extends Model implements OptionValueInterface
 {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
     protected $table = "product_option_values";
 }
