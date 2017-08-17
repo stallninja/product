2943c57f3e4156ca5af7243609f0a3dec9afebf2
Milos Dakic <milos.dakic@dt.com.au>
2017-08-17 21:25:30 +1000

    Inteerface definition

--- src/Contracts/Option.php
+++ src/Contracts/Option.php
@@ -2,6 +2,17 @@
 
 namespace StallNinja\Product\Contracts;
 
+use Illuminate\Database\Eloquent\Collection;
+
 interface Option
 {
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function values() : Collection;
+
+    /**
+     * @return bool
+     */
+    public function hasValues() : bool;
 }
--- src/Contracts/Variant.php
+++ src/Contracts/Variant.php
@@ -2,6 +2,18 @@
 
 namespace StallNinja\Product\Contracts;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
+
 interface Variant
 {
+    /**
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function product() : Model;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function optionValues() : Collection;
 }
